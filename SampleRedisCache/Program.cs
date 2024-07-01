102f2dff238d722f88ef4310498cf7e0a30a9186
Aatish Agarwal <aatishagarwal@gmail.com>
2024-07-01 21:57:59 +0530

    Uncommented secrets

--- SampleRedisCache/Program.cs
+++ SampleRedisCache/Program.cs
@@ -5,15 +5,14 @@ class Program
 {
     static void Main(string[] args)
     {
-        //*******Uncomment these lines*******
         // Replace these values with your Azure Redis Cache connection details
-        //string redisCacheName = "<your-redis-cache-name>";
-        //string redisCacheKey = "<your-redis-cache-key>";
-        //string redisCacheEndpoint = "<your-redis-cache-endpoint.redis.cache.windows.net>";
+        string redisCacheName = "<your-redis-cache-name>";
+        string redisCacheKey = "<your-redis-cache-key>";
+        string redisCacheEndpoint = "<your-redis-cache-endpoint.redis.cache.windows.net>";
 
         // Create the connection to Azure Redis Cache
-        ////var redisConnStr = $"{redisCacheName}.redis.cache.windows.net:6380,password={redisCacheKey},ssl=True,abortConnect=False";
-        ////var redis = ConnectionMultiplexer.Connect(redisConnStr);
+        var redisConnStr = $"{redisCacheName}.redis.cache.windows.net:6380,password={redisCacheKey},ssl=True,abortConnect=False";
+        var redis = ConnectionMultiplexer.Connect(redisConnStr);
 
         // Get a reference to the Redis database
         var db = redis.GetDatabase();
